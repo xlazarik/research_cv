6c1e778984d589350e946c861617e4f42891a180
Vladimir Lazarik <xlazarik@mail.muni.cz>
2024-05-23 11:16:06 +0200

    refactored main -> pub search

--- publications_search.py
+++ publications_search.py
@@ -42,46 +42,27 @@ def list_publications(researcher_name, output_folder):
         url = publication["pub_url"]
         #url = publication["bib"]['url'] if 'url' in publication["bib"] and url is None else None
 
-        # Create a filename with basic details and index
+
         filename = f"{year} >> {title}: {url}"
         files.append(filename)
         print(f"\rProcessed {i}/{len(publications)}")
 
-        #filepath = os.path.join(output_folder, filename)
+
     for file in sorted(files):
         print(file)
 
-        #journal = None
-        #journal = publication
+    #journal = None
+    #journal = publication
 
-        # Save publication information to a file
-        # Save publication information and download link (if available)
-        # with open(filepath, 'w', encoding='utf-8') as file:
-        #     file.write(f"Title: {title}\n")
-        #     file.write(f"Year: {year}\n")
-            # if journal:
-            #     file.write(f"Journal: {journal}\n")
-        #     if url:
-        #         try:
-        #             # Send a GET request without stream=True
-        #             response = requests.get(url)
-        #
-        #             # Check for successful download (status code 200)
-        #             if response.status_code == 200:
-        #                 with open(filepath, 'wb') as file:
-        #                     file.write(response.content)  # Write the entire content at once
-        #             else:
-        #                 print(f"Download failed for {url}. Status code: {response.status_code}")
-        #
-        #         except requests.exceptions.RequestException as e:
-        #             print(f"Error downloading {url}: {e}")
-        #     else:
-        #         print(f"No download URL available for {title}.")
-        #
-        # print(f"Saved publication information to: {filepath}")
+    filepath = os.path.join(output_folder, "output.txt")
+    with open(filepath, 'w', encoding='utf-8') as file:
+        for file_data in sorted(files):
+            file.write(file_data + "\n")
+
+    print(f"Saved publication information to: {filepath}")
 
 if __name__ == "__main__":
     researcher_name = "Barbora Kozlikova"
-    output_folder = "publications"
+    output_folder = "publications/"
 
     list_publications(researcher_name, output_folder)
